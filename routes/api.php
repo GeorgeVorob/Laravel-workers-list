b3e1d0c147838632a35206cdf6d7335b16389e98
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-11-23 17:31:40 +0600

    Remove specialization controller
    Специализации только заполняются в миграции при создании их таблицы

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('specs', SpecController::class)
-    ->only(['index', 'store']);
-
 Route::apiResource('workers', workers_api::class);
