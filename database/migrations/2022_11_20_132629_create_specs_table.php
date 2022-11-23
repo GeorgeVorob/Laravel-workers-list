e84548f9ce49245a169be04fdb11d7df8deab0ac
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-11-23 15:17:56 +0600

    Fill specialization with default data on migration

--- database/migrations/2022_11_20_132629_create_specs_table.php
+++ database/migrations/2022_11_20_132629_create_specs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Spec;
 
 return new class extends Migration
 {
@@ -18,6 +19,17 @@ return new class extends Migration
             $table->string("name");
             $table->timestamps();
         });
+
+        $spec1 = new Spec();
+        $spec1->name = "software developer";
+        $spec2 = new Spec();
+        $spec2->name = "QA Engineer";
+        $spec3 = new Spec();
+        $spec3->name = "project manager";
+
+        $spec1->save();
+        $spec2->save();
+        $spec3->save();
     }
 
     /**
