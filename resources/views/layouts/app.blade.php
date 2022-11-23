e5bcbba4de625015e76a2008bf6a27b7cc28f923
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-11-23 14:06:09 +0600

    Create worker form markdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
--- resources/views/workers/index.blade.php
+++ resources/views/workers/index.blade.php
@@ -2,25 +2,33 @@
 
     <form method="POST" action="{{ route('workers.store') }}" enctype="multipart/form-data">
         @csrf
-        <label>Имя</label>
-        <input name="name">
-        </br>
-        <label>Описание</label>
-        <textarea name="desc"></textarea>
-        </br>
-        <label>Фото</label>
-        <input type="file" name="image" />
-        </br>
-        <select name='spec_id'>
-            @foreach ($specs as $spec)
-            <option value="{{$spec->id}}">{{$spec->name}}</option>
-            @endforeach
-        </select>
-        <button type="submit">Create worker</button>
+        <div class="form-group">
+            <div class="mb-2">
+                <label class="form-label">Имя</label>
+                <input name="name" placeholder="Имя работника" class="form-control" required>
+            </div>
+            <div class="mb-2">
+                <label class="form-label">Описание</label>
+                <textarea name="desc" class="form-control"></textarea>
+            </div>
+            <div class="mb-2">
+                <label class="form-label">Фото</label>
+                <input type="file" name="image" />
+            </div>
+            <div class="mb-2">
+                <label class="form-label">Специализация</label>
+                <select name='spec_id' class="form-select mb-2">
+                    @foreach ($specs as $spec)
+                    <option value="{{$spec->id}}">{{$spec->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Create worker</button>
+        </div>
     </form>
+    <hr>
 
     @foreach ($workers as $worker)
-    <hr>
     <div>
         <img src=" {{Storage::url($worker->image)}}" alt="nop" style="height: 100px;width: 100px;">
         <h2>Имя: {{$worker->name}}</h2>
@@ -35,5 +43,6 @@
             <button type="submit">Delete</button>
         </form>
     </div>
+    <hr>
     @endforeach
 </x-app-layout>
